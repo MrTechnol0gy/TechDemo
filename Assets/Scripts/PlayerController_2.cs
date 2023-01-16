a67bafb6d8d110e710733e59c57bb6f937d3efff
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2023-01-15 22:40:49 -0400

    Update PlayerController_2.cs
    further refinement/attempts at a look system

--- Assets/Scripts/PlayerController_2.cs
+++ Assets/Scripts/PlayerController_2.cs
@@ -6,19 +6,19 @@ using UnityEngine.InputSystem;
 public class PlayerController_2 : MonoBehaviour
 {
     PlayerController controls;
-    Vector2 move;
-    Vector2 mouseInput;
+    Vector2 m_Move;
+    Vector2 m_Look;
 
     [SerializeField] float moveSpeed = 10f;
 
     void Awake()
     {
         controls = new PlayerController();
-        controls.Player.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
-        controls.Player.Move.canceled += ctx => move = Vector2.zero;
+        controls.Player.Move.performed += ctx => m_Move = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => m_Move = Vector2.zero;
         //mouse look
-        //controls.Player.Look.performed += ctx => mouseInput.x = ctx.ReadValue<float>();
-        //controls.Player.Look.performed += ctx => mouseInput.y = ctx.ReadValue<float>();
+        //controls.Player.Look.performed += ctx => m_Look.x = ctx.ReadValue<float>();
+        //controls.Player.Look.performed += ctx => m_Look.y = ctx.ReadValue<float>();
     }
 
     private void OnEnable()
@@ -29,20 +29,26 @@ public class PlayerController_2 : MonoBehaviour
     private void OnDisable()
     {
         controls.Player.Disable();
-    }    
+    }
 
-    // Start is called before the first frame update
-    void Start()
+    public void OnMove(InputValue value)
     {
-        
+        m_Move = value.Get<Vector2>();
+    }
+
+    public void OnLook(InputValue value)
+    {
+        m_Look = value.Get<Vector2>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(move.x, 0.0f, move.y) * moveSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(m_Move.x, 0.0f, m_Move.y) * moveSpeed * Time.deltaTime;
         transform.Translate(movement, Space.World);
         //mouse look        
-        //Vector2 look = new Vector2(mouseInput.x, mouseInput.y);
+        Vector2 look = new Vector2(m_Look.x, m_Look.y) * Time.deltaTime;
+        transform.Rotate(look, Space.World); //so close, but so wrong - should this be on the camera instead?
+        //transform.localRotation = look;
     }
 }
