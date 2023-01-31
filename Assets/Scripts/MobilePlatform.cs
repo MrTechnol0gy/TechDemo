c4e28a6415947476bbe4ad53f6e9553b6e303ef7
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2023-01-31 10:29:33 -0400

    Moving Platform begun

---
+++ Assets/Scripts/MobilePlatform.cs
@@ -0,0 +1,139 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MobilePlatform : MonoBehaviour
+{
+
+    private float _startPosition;
+    private float t = 0.0f;
+    private bool platformRise = true;
+    private bool platformFall = false;
+    private bool platformLeft = true;
+    private bool platformRight = false;
+    private bool platformForward = true;
+    private bool platformBackward = false;
+
+    [SerializeField] GameObject platform;
+    [SerializeField] float platformDistance = 5.0f; 
+    [SerializeField] bool UpDown = false;
+    [SerializeField] bool LeftRight = false;   
+    [SerializeField] bool ForwardBackward = false;
+    [SerializeField] int delay = 0; //doesn't work above zero, introduces stutters
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (UpDown == true)
+        {
+            if (platformRise == true)
+            {
+            Invoke("PlatformUp", delay);
+            }
+            else if (platformFall == true)
+            {
+                Invoke("PlatformDown", delay);
+            }
+        }
+        else if (LeftRight == true)
+        {
+            if (platformLeft == true)
+            {
+                Invoke("PlatformLeft", delay);
+            }
+            else if (platformRight == true)
+            {
+                Invoke("PlatformRight", delay);
+            }
+        }
+        else if (ForwardBackward == true)
+        {
+            if (platformForward == true)
+            {
+                Invoke("PlatformForward", delay);
+            }
+            else if (platformBackward == true)
+            {
+                Invoke("PlatformBackward", delay);
+            }
+        }
+    }
+    void PlatformUp()
+    {
+            t += 0.5f * Time.deltaTime;
+            float distance = Mathf.SmoothStep(_startPosition, platformDistance, t);
+            platform.transform.localPosition = new Vector3(0, distance, 0);
+            if (t > 1.0f)
+            {
+                platformRise = false;
+                t = 0.0f;
+                platformFall = true;
+            }
+    }
+    void PlatformDown()
+    {
+            t += 0.5f * Time.deltaTime;
+            float distance = Mathf.SmoothStep(platformDistance, _startPosition, t);
+            platform.transform.localPosition = new Vector3(0, distance, 0);
+            if (t > 1.0f)
+            {
+                platformFall = false;
+                t = 0.0f;
+                platformRise = true;
+            }
+    }
+    void PlatformLeft()
+    {
+            t += 0.5f * Time.deltaTime;
+            float distance = Mathf.SmoothStep(_startPosition, platformDistance, t);
+            platform.transform.localPosition = new Vector3(distance, 0, 0);
+            if (t > 1.0f)
+            {
+                platformLeft = false;
+                t = 0.0f;
+                platformRight = true;
+            }
+    }
+    void PlatformRight()
+    {
+            t += 0.5f * Time.deltaTime;
+            float distance = Mathf.SmoothStep(platformDistance, _startPosition, t);
+            platform.transform.localPosition = new Vector3(distance, 0, 0);
+            if (t > 1.0f)
+            {
+                platformRight = false;
+                t = 0.0f;
+                platformLeft = true;
+            }
+    }
+    void PlatformForward()
+    {
+            t += 0.5f * Time.deltaTime;
+            float distance = Mathf.SmoothStep(_startPosition, platformDistance, t);
+            platform.transform.localPosition = new Vector3(0, 0, distance);
+            if (t > 1.0f)
+            {
+                platformForward = false;
+                t = 0.0f;
+                platformBackward = true;
+            }
+    }
+    void PlatformBackward()
+    {
+            t += 0.5f * Time.deltaTime;
+            float distance = Mathf.SmoothStep(platformDistance, _startPosition, t);
+            platform.transform.localPosition = new Vector3(0, 0, distance);
+            if (t > 1.0f)
+            {
+                platformBackward = false;
+                t = 0.0f;
+                platformForward = true;
+            }
+    }
+}
