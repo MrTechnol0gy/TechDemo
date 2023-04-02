da2f09c748aba8d1ab1e54115fedb5fb5cc088c5
Richard Clarke <113057777+MrTechnol0gy@users.noreply.github.com>
2023-04-02 19:32:29 -0300

    state machine for doors added

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System.Diagnostics;
-//using System.Diagnostics;
 using System.Threading;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    enum DoorState
+    {
+        open,           // Door is fully open = 0        
+        closed,         // Door is fully closed = 1
+        opening,        // Door is in the process of opening = 2
+        closing         // Door is in the process of closing = 3
+    }
     [Header("Is this door a single or a double door? Choose one.")]
     [SerializeField] bool Single_door = false;
     [SerializeField] bool Double_door = false;
@@ -19,10 +24,7 @@ public class Door : MonoBehaviour
     private float maxAngle = 90.0f;
     private float t = 0.0f;
 
-    private bool isLerpOpenSingle = false;
-    private bool isLerpCloseSingle = false;
-    private bool isLerpOpenDouble = false;
-    private bool isLerpCloseDouble = false;    
+    DoorState doorState = DoorState.closed;
 
     // Start is called before the first frame update
     void Start()
@@ -31,46 +33,71 @@ public class Door : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        if (isLerpOpenSingle == true)
-        {
-            OpenSingleDoor();            
-        }
-        if (isLerpCloseSingle == true)
-        {
-            CloseSingleDoor();            
-        }
-        if (isLerpOpenDouble == true)
-        {            
-            OpenDoubleDoor();
-        }
-        if (isLerpCloseDouble == true)
-        {
-            CloseDoubleDoor();            
-        }
-    }
+        StateProceed();
+    }    
 
-    void OnTriggerEnter(Collider other) //opens the door when the player enters the collider area
+    private void StateProceed()
     {
-        if (other.gameObject.CompareTag("Player") && Single_door == true)
-        {            
-            isLerpOpenSingle = true;
-        }
-        else if (other.gameObject.CompareTag("Player") && Double_door == true)
-        {            
-            isLerpOpenDouble = true;
+        switch (doorState)
+        {
+            case DoorState.opening:
+            {
+                if (Single_door == true)
+                {
+                    OpenSingleDoor();
+                }
+                else if (Double_door == true)
+                {
+                    OpenDoubleDoor();
+                }
+            }
+            break;
+            case DoorState.closing:
+            {
+                if (Single_door == true)
+                {
+                    CloseSingleDoor();
+                }
+                else if (Double_door == true)
+                {
+                    CloseDoubleDoor();
+                }
+            }
+            break;
+            default:
+            break;
         }
     }
-    void OnTriggerExit(Collider other) //closes the door when the player exits the collider area
+    IEnumerator OnTriggerEnter(Collider other) //opens the door when the player enters the collider area
     {
-        if (other.gameObject.CompareTag("Player") && Single_door == true)
+        if (other.gameObject.CompareTag("Player"))
         {
-            isLerpCloseSingle = true;
-        }
-        else if (other.gameObject.CompareTag("Player") && Double_door == true)
+            if (doorState != DoorState.closing)
+            {
+                doorState = DoorState.opening;
+            }
+            else 
+            {
+                yield return new WaitUntil(() => doorState == DoorState.closed);
+                doorState = DoorState.opening;
+            }
+        }        
+    }
+    IEnumerator OnTriggerExit(Collider other) //closes the door when the player exits the collider area
+    {
+        if (other.gameObject.CompareTag("Player"))
         {
-            isLerpCloseDouble = true;            
+            if (doorState != DoorState.opening)
+            {
+                doorState = DoorState.closing;
+            }
+            else
+            {
+                yield return new WaitUntil(() => doorState == DoorState.open);
+                doorState = DoorState.closing;
+            }
         }
     }
 
@@ -81,7 +108,7 @@ public class Door : MonoBehaviour
         Hinge_One.transform.localEulerAngles = new Vector3(0, angle, 0);
         if (t > 1.0f)
         {
-            isLerpOpenSingle = false;
+            doorState = DoorState.open;
             t = 0.0f;
         }
     }
@@ -93,7 +120,7 @@ public class Door : MonoBehaviour
         Hinge_One.transform.localEulerAngles = new Vector3(0, angle, 0);
         if (t > 1.0f)
         {
-            isLerpCloseSingle = false;
+            doorState = DoorState.closed;
             t = 0.0f;
         }
     }
@@ -105,7 +132,7 @@ public class Door : MonoBehaviour
         Hinge_Two.transform.localEulerAngles = new Vector3(0, -angle, 0); 
         if (t > 1.0f)
         {
-            isLerpOpenDouble = false;
+            doorState = DoorState.open;
             t = 0.0f;
         }
     }
@@ -118,7 +145,7 @@ public class Door : MonoBehaviour
         Hinge_Two.transform.localEulerAngles = new Vector3(0, -angle, 0);
         if (t > 1.0f)
         {
-            isLerpCloseDouble = false;
+            doorState = DoorState.closed;
             t = 0.0f;
         }
     }
